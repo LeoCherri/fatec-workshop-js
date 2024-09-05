17b8714ab3263fd150bfe68a457928fe80efdd11
LeoCherri <deltonnardy1245@gmail.com>
2024-09-05 15:17:28 -0300

    Resoluçao desafio Workshop

---
+++ desafio.js
@@ -0,0 +1,10 @@
+//Nome: Leonardo Cherri Rodrigues
+//2° Semestre DSM
+//Desafio workshop FATEC JAHU
+
+const x = 5;
+const y = 10;
+function somar(a, b){
+    return a + b;
+} 
+console.log(somar(x, y));
